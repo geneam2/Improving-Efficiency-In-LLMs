fc8aede2439816c07a7bccdc85f9e60800d9ebd9
Gene Lam <genecodes2@gmail.com>
2024-11-18 17:21:15 -0500

    trained model on google colab

--- main.py
+++
@@ -1,9 +0,0 @@
-from utils import device
-import torch
-
-# Example usage
-print(f"Using device: {device}")
-
-# Creating tensors on the appropriate device
-tensor = torch.tensor([1, 2, 3]).to(device)
-print(tensor)
